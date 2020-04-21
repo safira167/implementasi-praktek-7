94a81c89a475617cb81ecb4678a0c6382aba9209
Nanda Okta Mayangsari <63951306+nandaomay@users.noreply.github.com>
2020-04-21 11:34:37 +0700

    Update pertemuan_7.js

--- pertemuan_7.js
+++ pertemuan_7.js
@@ -152,3 +152,137 @@ context.stroke();
 
 context.stroke();
 context.strokeRect(520, 50, 50, 50);
+
+//gambar 3
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.lineWidth = 10; 
+ctx.beginPath();
+ctx.moveTo(997, 450);
+ctx.lineTo(1203, 450);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(1000, 450);
+ctx.bezierCurveTo(900, 400, 900, 270, 1030, 270);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(1200, 450);
+ctx.bezierCurveTo(1300, 400, 1300, 270, 1170, 270);
+ctx.stroke();
+
+var mycan = document.getElementById("myCanvas");
+var context = mycan.getContext("2d");
+var rectWidth = 80;
+var rectHeight = 125;
+rectX = 1090;
+rectY = 150;
+var cornerRadius = 50;
+
+context.beginPath();
+context.moveTo(rectX, rectY);
+context.lineTo(rectX+rectWidth-cornerRadius, rectY);
+context.arcTo(rectX+rectWidth, rectY, rectX+rectWidth, rectY+cornerRadius, cornerRadius);
+context.lineTo(rectX+rectWidth, rectY+rectHeight);
+context.lineWidth = 10;
+context.stroke();
+
+var mycan = document.getElementById("myCanvas");
+var context = mycan.getContext("2d");
+var rectWidth = 80;
+var rectHeight = 125;
+rectX = 1110;
+rectY = 150;
+var cornerRadius = 50;
+
+context.beginPath();
+context.moveTo(rectX, rectY);
+context.lineTo(rectX-rectWidth+cornerRadius, rectY);
+context.arcTo(rectX-rectWidth, rectY, rectX-rectWidth, rectY+cornerRadius, cornerRadius);
+context.lineTo(rectX-rectWidth, rectY+rectHeight);
+context.lineWidth = 10;
+context.fillStyle = "#DCDCDC";
+context.fill();
+context.stroke();
+
+context.beginPath();
+context.moveTo(1170, 200);
+context.lineTo(1170, 115);
+context.stroke();
+
+context.lineWidth = 10;
+context.beginPath();
+context.moveTo(1170, 120);
+context.lineTo(1250, 120);
+context.lineTo(1170, 50);
+context.lineJoin = 'round';
+context.stroke();
+
+context.beginPath();
+context.moveTo(1173, 50);
+context.lineTo(970, 50);
+context.lineTo(970, 100);
+context.lineTo(1045, 165);
+context.fillStyle = "black";
+context.fill();
+
+// context.beginPath();
+// context.moveTo(992, 115);
+// context.lineTo(920, 200);
+// context.lineTo(970, 210);
+// context.lineTo(1020, 150);
+// context.stroke();
+
+
+context.strokeRect(920, 50, 50, 50);
+context.stroke();
+
+
+// context.beginPath();
+// context.moveTo(592, 115);
+// context.lineTo(550, 230);
+// context.lineTo(600, 230);
+// context.lineTo(620, 150);
+// context.lineWidth = 2;
+// context.stroke();
+
+
+var rect1 = { x: 160, y: 110, width: 35, height: 105 };
+var rect2 = { x: 530, y: -50, width: 35, height: 105 };
+var rect3 = { x: 508, y: -60, width: 35, height: 105 };
+var rect4 = { x: 518, y: -60, width: 35, height: 120 };
+var rect5 = { x: 885, y: -280, width: 35, height: 105 };
+
+context.fillStyle = 'black';
+ctx.translate(rect1.x + rect1.width / 2, rect1.y + rect1.height / 2);
+ctx.rotate(Math.PI / 7);
+ctx.translate(-rect1.x - rect1.width / 2, -rect1.y - rect1.height / 2);
+ctx.fillRect(rect1.x, rect1.y, rect1.width, rect1.height);
+
+ctx.translate(rect2.x + rect2.width / 2, rect2.y + rect2.height / 2);
+ctx.rotate(Math.PI / -15);
+ctx.translate(-rect2.x - rect2.width / 2, -rect2.y - rect2.height / 2);
+ctx.strokeRect(rect2.x, rect2.y, rect2.width, rect2.height);
+ctx.lineWidth = 3;
+ctx.stroke();
+
+ctx.translate(rect3.x + rect3.width / 2, rect3.y + rect3.height / 2);
+ctx.rotate(Math.PI / 7);
+ctx.translate(-rect3.x - rect3.width / 2, -rect3.y - rect3.height / 2);
+ctx.strokeRect(rect3.x, rect3.y, rect3.width, rect3.height);
+ctx.lineWidth = 3;
+ctx.stroke();
+
+context.fillStyle = 'black';
+ctx.translate(rect4.x + rect4.width / 2, rect4.y + rect4.height / 2);
+ctx.rotate(Math.PI / -20);
+ctx.translate(-rect4.x - rect4.width / 2, -rect4.y - rect4.height / 2);
+ctx.fillRect(rect4.x, rect4.y, rect4.width, rect4.height);
+
+context.fillStyle = 'black';
+ctx.translate(rect5.x + rect5.width / 2, rect5.y + rect5.height / 2);
+ctx.rotate(Math.PI / -6);
+ctx.translate(-rect5.x - rect5.width / 2, -rect5.y - rect5.height / 2);
+ctx.fillRect(rect5.x, rect5.y, rect5.width, rect5.height);
